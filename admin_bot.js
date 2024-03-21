9215b34bd0cc221b5ae0efbdbb88a1e7817e3345
Кеворков Норайр Сергеевич <n.kevorkov@maxmarket24.ru>
2024-03-21 22:57:45 +0300

    4242

--- admin_bot.js
+++ admin_bot.js
@@ -13,21 +13,24 @@ function getAdminPerms(chatId, cb) {
 
 module.exports.startAdminBot = function startAdminBot() {
     adminBot.on('message', async msg => {
-        getAdminPerms(adminBot, msg.chat.id, () => {
+        getAdminPerms(msg.chat.id, () => {
             adminBot.sendMessage(ADMIN_USER_ID, 'Welcome', {
                 reply_markup: {
                     inline_keyboard: [[{text: 'Создать семинар', callback_data: 'create_event'}]]
                 }
             });
+            adminBot.on('callback_query', query => {
+                console.log(query);
+                // getAdminPerms(msg.from.id, async () => {
+                //     console.log(msg);
+                //     if (msg.data === 'create_event') {
+                //         await adminBot.sendMessage(msg.from.id, '42')
+                //     }
+                // })
+            })
         })
     });
 
-    adminBot.on('callback_query', msg => {
-        getAdminPerms(adminBot, msg.from.id, async () => {
-            if (msg.data === 'create_event') {
-                await adminBot.sendMessage(msg.from.id, '42')
-            }
-        })
-    })
+
 }
 module.exports.adminBot = adminBot;
